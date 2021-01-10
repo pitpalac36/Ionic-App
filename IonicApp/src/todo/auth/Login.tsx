bcb535dc7ed618c345567241bd4838b5d7e36834
Dunca Laura <lauradunca1005@gmail.com>
2021-01-10 12:46:19 +0200

    override existing component animations

--- IonicApp/src/todo/auth/Login.tsx
+++ IonicApp/src/todo/auth/Login.tsx
@@ -32,29 +32,6 @@ export const Login: React.FC<RouteComponentProps> = ({ history }) => {
     return <Redirect to={{ pathname: '/' }} />
   }
 
-  const enterAnimation = (baseEl: any) => {
-    const backdropAnimation = createAnimation()
-        .addElement(baseEl.querySelector('ion-backdrop')!)
-        .fromTo('opacity', '0.01', 'var(--backdrop-opacity)');
-
-    const wrapperAnimation = createAnimation()
-        .addElement(baseEl.querySelector('.modal-wrapper')!)
-        .keyframes([
-            { offset: 0, opacity: '0', transform: 'scale(0)' },
-            { offset: 1, opacity: '0.99', transform: 'scale(1)' }
-        ]);
-
-    return createAnimation()
-        .addElement(baseEl)
-        .easing('ease-out')
-        .duration(500)
-        .addAnimation([backdropAnimation, wrapperAnimation]);
-}
-
-const leaveAnimation = (baseEl: any) => {
-    return enterAnimation(baseEl).direction('reverse');
-}
-
   return (
     <IonPage>
       <IonHeader>
--- IonicApp/src/todo/book/Book.tsx
+++ IonicApp/src/todo/book/Book.tsx
@@ -1,5 +1,5 @@
-import { IonItem, IonLabel } from "@ionic/react";
-import React from "react";
+import { createAnimation, IonItem, IonLabel, IonModal } from "@ionic/react";
+import React, { useEffect, useState } from "react";
 import {BookProps} from "./BookProps";
 
 interface BookPropsExtended extends BookProps {
@@ -7,8 +7,47 @@ interface BookPropsExtended extends BookProps {
 }
 
 const Book: React.FC<BookPropsExtended> = ({_id, title, genre, startedReading, finishedReading, latitude, longitude, webViewPath, onEdit}) => {
+
+    const [showModal, setShowModal] = useState(false);
+
+    useEffect(() => {
+        document.getElementById("image")!.addEventListener('mouseenter', () => {
+            setShowModal(true);
+        });
+    }, [document.getElementById("image")]);
+
+    useEffect(() => {
+        document.getElementById("item")!.addEventListener('click', () => {
+            onEdit(_id);
+        });
+    }, [document.getElementById("item")]);
+
+    const enterAnimation = (baseEl: any) => {
+
+        const backdropAnimation = createAnimation()
+            .addElement(baseEl.querySelector('ion-backdrop')!)
+            .fromTo('opacity', '0.01', 'var(--backdrop-opacity)');
+    
+        const wrapperAnimation = createAnimation()
+            .addElement(baseEl.querySelector('.modal-wrapper')!)
+            .keyframes([
+                { offset: 0, opacity: '0', transform: 'scale(0)' },
+                { offset: 1, opacity: '0.99', transform: 'scale(1)' }
+            ]);
+    
+        return createAnimation()
+            .addElement(baseEl)
+            .easing('ease-out')
+            .duration(500)
+            .addAnimation([backdropAnimation, wrapperAnimation]);
+    }
+    
+    const leaveAnimation = (baseEl: any) => {
+        return enterAnimation(baseEl).direction('reverse');
+    }
+
     return (
-        <IonItem onClick={ () => onEdit(_id) }>
+        <IonItem id="item">
             <IonLabel>{_id}</IonLabel>
             <IonLabel>{title}</IonLabel>
             <IonLabel>{genre}</IonLabel>
@@ -16,8 +55,15 @@ const Book: React.FC<BookPropsExtended> = ({_id, title, genre, startedReading, f
             <IonLabel>{finishedReading.toString()}</IonLabel>
             <IonLabel>{latitude}</IonLabel>
             <IonLabel>{longitude}</IonLabel>
-            {webViewPath && (<img src={webViewPath} width={'100px'} height={'100px'}/>)}
-            {!webViewPath && (<img src={'https://static.thenounproject.com/png/187803-200.png'} width={'100px'} height={'100px'}/>)}
+            {webViewPath && (<img id="image" src={webViewPath} width={'100px'} height={'100px'} onClick={() => {
+                setShowModal(true);
+            }} />)}
+
+            <IonModal isOpen={showModal} enterAnimation={enterAnimation} leaveAnimation={leaveAnimation} backdropDismiss={true} onDidDismiss={() => setShowModal(false)}>
+                <img id="image" src={webViewPath} />
+            </IonModal>
+
+            {!webViewPath && (<img src={'https://static.thenounproject.com/png/187803-200.png'} width={'100px'} height={'100px'} />)}
         </IonItem>
     )
 };
